c8ef413b7a97c2b204393b0f8cf1c2fcb66f2935
jdh5451 <jdh5451@rit.edu>
2021-06-09 15:04:40 -0400

    lab 4 complete

---
+++ StringConcatenation.java
@@ -0,0 +1,24 @@
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		String beginning="Hello";
+		
+		String end=" World";
+		
+		String combo = beginning+end;
+		
+		System.out.println(combo);
+		
+		long l=203L;
+		System.out.println("The value of l: " +l);
+		
+		boolean b=true;
+		System.out.println(b+" is true");
+		
+		int x=10;
+		int y=33;
+		System.out.println(x + y + " is the sum of x and y");
+	}
+
+}
