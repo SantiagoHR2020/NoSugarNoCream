0b9ca6e2af24a0ac366acc367421f555d8aaceb8
jdh5451 <jdh5451@rit.edu>
2021-06-09 15:08:15 -0400

    lab 5 pushed

---
+++ ConditionalBlocks.java
@@ -0,0 +1,14 @@
+
+public class ConditionalBlocks {
+
+	public static void main(String[] args) {
+		boolean b=true;
+		
+		
+		if(b) {
+			System.out.println("inside the if");
+		}
+		System.out.println("outside the if");
+	}
+
+}
