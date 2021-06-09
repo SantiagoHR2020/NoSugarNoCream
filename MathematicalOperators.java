96e0ad50e7b8d29dc3d4b720fb084719d17097db
Santiago Hincapie <santiagohr81@hotmail.com>
2021-06-09 15:01:19 -0400

    lab 1 and 2

---
+++ MathematicalOperators.java
@@ -0,0 +1,34 @@
+
+public class MathematicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x = 4;
+        int y = 2;
+        int z;
+        
+        z = x+y;
+        
+        System.out.println(z);
+        
+        z = x-y;
+        
+        System.out.println(z);
+        
+        z = x*y;
+        
+        System.out.println(z);
+        
+        z = x/y;
+        
+        System.out.println(z);
+        
+        z = x%y;
+        
+        System.out.println(z);
+        
+        
+
+	}
+
+}
