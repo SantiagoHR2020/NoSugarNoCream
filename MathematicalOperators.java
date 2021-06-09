8110efae8da320f5ab8c4e314aa7449b28d3abe6
jdh5451 <jdh5451@rit.edu>
2021-06-09 14:51:55 -0400

    lab 2 done

---
+++ MathematicalOperators.java
@@ -0,0 +1,30 @@
+
+public class MathematicalOperators {
+
+	public static void main(String[] args) {
+		int x=5;
+		int y=3;
+		int z;
+		
+		z=x+y;
+		System.out.println(z);
+		
+		z=x-y;
+		System.out.println(z);
+		
+		z=x*y;
+		System.out.println(z);
+		
+		z=x/y;
+		System.out.println(z);
+		
+		z=x%y;
+		System.out.println(z);
+		
+		float f=3.6f;
+		int i=22;
+		double result=f+i;
+		System.out.println(result);
+	}
+
+}
