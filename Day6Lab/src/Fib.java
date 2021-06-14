4a19bda9a2d5455c44e0d9065b36eda2be4a18c3
jgriffin01 <jgriffin01@email.wm.edu>
2021-06-14 12:52:53 -0400

    Day6 Labs

---
+++ Day6Lab/src/Fib.java
@@ -0,0 +1,28 @@
+
+public class Fib {
+	public int fibonacci(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		}
+		else {
+			return (fibonacci(n-1) + fibonacci(n-2));
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Fib fib = new Fib();
+		
+		int result = fib.fibonacci(2);
+		System.out.println(result);
+		
+		result = fib.fibonacci(5);
+		System.out.println(result);
+		
+		result = fib.fibonacci(10);
+		System.out.println(result);
+		}
+}
---
+++ Day6Lab/src/LinearSearch.java
@@ -0,0 +1,21 @@
+
+public class LinearSearch {
+	public static void main(String[] args) {
+		char letter = 'm';
+		char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		LinearSearch ls = new LinearSearch();
+		System.out.println(ls.findLetter(letter, letters));
+	}
+	
+	public int findLetter(char target, char[] data) {
+		int result = -1;
+		
+		for (int i = 0; i < data.length; i++) {
+			char temp = data[i];
+			if (temp == target) {
+				return i;
+			}
+		}
+		return result;
+	}
+}
